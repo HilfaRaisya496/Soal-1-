ce6ace6af1d40bf91173a31868b907deeeba70c0
HilfaRaisya496 <145620634+HilfaRaisya496@users.noreply.github.com>
2025-04-11 22:08:04 +0700

    Add files via upload

---
+++ Soal1/soal4.php
@@ -0,0 +1,12 @@
+<!-- VARIABEL -->
+<?php
+ $nama = "Hilfa Raisya Eksa Putra" ;
+ $umur = "18 Tahun" ;
+ $hobi = "Futsal" ;
+ $status = "Single" ;
+?>
+
+<!-- OUTPUT -->
+<?php echo "Nama Lengkap : " . $nama . "<br>" . "Umur : " . $umur . "<br>" . "Hobi : " . $hobi . "<br>" . "Status : " . $status ; ?>
+
+
